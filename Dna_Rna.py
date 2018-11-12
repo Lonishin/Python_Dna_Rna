3753477e994bd48480fbed47c8b74ee0bd9b0dbc
Liubov Lonishin <43217334+Lonishin@users.noreply.github.com>
2018-11-12 15:12:21 +0300

    Update Dna_Rna.py
    Second version of programme

--- Dna_Rna.py
+++ Dna_Rna.py
@@ -1,7 +1,11 @@
 class Rna:
     def __init__(self, seq):
-        self.sequence = seq
+        self.sequence = seq.upper()
         self.complement = ""
+        if 'A' or 'C' or 'G' or 'T' in self.sequence:
+            pass
+        else:
+            raise 'This is not a Rna'
 
     def reverse_complement(self):
         complement_dictionary = {'A': 'U', 'C': 'G', 'U': 'A', 'G': 'C'}
@@ -12,17 +16,33 @@ class Rna:
 
     def gc(self):
         gc_amount = (self.sequence.count("C") + self.sequence.count("G")) / len(self.sequence)
-        return gc_amount
+        return round(gc_amount, 2)
 
     def __repr__(self):
         return self.sequence
 
 
-class Dna(Rna):
-    def __init__(self, seq):
-        self.dna_seq = seq
+class Dna:
+    def __init__(self, sequence):
+        self.sequence = sequence.upper()
+        self.complement = ""
+
+        if 'A' or 'C' or 'G' or 'T' in self.sequence:
+            pass
+        else:
+            raise 'This is not a Dna'
 
     def transcribe(self):
-        rna_seq = self.dna_seq.replace('T', 'U')
-        rna_complement = Rna(rna_seq).reverse_complement()
+        rna_complement = Rna(self.sequence.replace('T', 'U'))
         return rna_complement
+
+    def gc(self):
+        gc_amount = (self.sequence.count("C") + self.sequence.count("G")) / len(self.sequence)
+        return round(gc_amount, 2)
+
+    def reverse_complement(self):
+        complement_dictionary = {'A': 'T', 'C': 'G', 'T': 'A', 'G': 'C'}
+
+        for i in range(len(self.sequence)):
+            self.complement += complement_dictionary[self.sequence[i]]
+        return self.complement[::-1]
